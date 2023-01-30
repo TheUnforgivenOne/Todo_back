b7035ad404f4fbf4de1f3219ce90fbc318cf44aa
Vlad <vladpotapow@yandex.ru>
2023-01-30 19:54:32 +0300

    Initialized dotenv

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+import * as dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import todosRouter from './Todo/router.js';
 
+dotenv.config({ path: '.env.local' });
+dotenv.config();
+
 const app = express();
-const port = 5000;
 
 app.use(express.json());
 app.use(todosRouter);
@@ -11,11 +14,12 @@ app.use(todosRouter);
 const start = async () => {
   try {
     await mongoose.connect(
-      'mongodb+srv://admin:admin@cluster0.nj6djyg.mongodb.net/?retryWrites=true&w=majority'
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nj6djyg.mongodb.net/?retryWrites=true&w=majority`
     );
-    app.listen(port, () => {
-      console.log(`Server running on port ${port}`);
-    });
+
+    app.listen(process.env.PORT, () =>
+      console.log(`Server running on port ${process.env.PORT}`)
+    );
   } catch (e) {
     throw e;
   }
